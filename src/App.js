deaab47c410fc7ce65e38fb0acee164b84246f37
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-09-07 18:26:08 +0500

    button comp cl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import NavbarComp from "./components/Navbar";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <NavbarComp />
-    </div>
+    <>
+      <Router>
+        <NavbarComp />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
